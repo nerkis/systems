6cdc54ab3a7bae2134bcc866c608eb137c505aca
Nicole Erkis <nicoleerkis@gmail.com>
2011-09-24 13:02:14 -0400

    trying to add C warmup

---
+++ studentData.c
@@ -0,0 +1,22 @@
+/*studentData is a program that allows a user to enter and manipulate information about students (last name, ID number, letter grade)
+
+TODO: 1. create space in memory for an array of n students
+      2. fill the array with user-provided student information
+      3. print the student information on the screen
+      4. count the number of each letter grade and print totals
+      5. release the array space when finished*/
+
+//import statments
+#import <stdio.h>
+
+//funtion prototypes
+
+
+//main method
+int main(void){
+printf("My first C program\n");
+return 0;
+} 
+
+//additional functions
+
