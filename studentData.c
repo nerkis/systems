10c7549c88184f251c21ce4b4e37bf2cdf40db10
Nicole Erkis <nicoleerkis@gmail.com>
2011-09-24 14:12:53 -0400

    started user input

--- studentData.c
+++ studentData.c
@@ -12,9 +12,23 @@ TODO: 1. create space in memory for an array of n students
 //main method
 int main(void){
 
+//vars
+int *studArray;
+int numStudents;
+
+//use in for loop
+int i;
+
 printf("STUDENT CONTROL PROGRAM\n\nThis program will:\n1. Create space for an array of n students\n2. Fill the aray with user-provided student information\n3. Print all student information on the screen\n4. Count the number of As, Bs, Cs, Ds, and Fs\n5. Release the array space\n\nEnter the array size or 0 if you want to exit:\n");
 
 //take user input and calloc an array of that size
+scanf("%d", &numStudents);
+studArray = (int*)calloc(numStudents, sizeof(int));
+
+for(i = 0; i < numStudents; i++)
+{
+	//do stuff
+}
 
 //for the number students...
 //create a student struct
