d6df84d1a02304b9e5946f8718136c3c786fbfae
Nicole Erkis <nicoleerkis@gmail.com>
2011-09-25 14:20:29 -0400

    put files in a folder

---
+++ student/makefile
@@ -0,0 +1,8 @@
+# structure:
+# target: requirements
+#	target build instructions
+
+tudent: studentData.c
+	gcc -o student studentData.c -I.
+clean:
+	rm *.o
---
+++ student/studentData.h
@@ -0,0 +1,16 @@
+/*studentData.h contains all function declarations and data structures for the studentData.c program*/
+
+//student struct
+struct student {
+	char name[18];
+	int idNum;
+	char grade;
+};
+
+//function prototypes
+void flush_stdin();
+void incrementGrade(char c);
+void printGrades();
+void createStudents(struct student *s, int i);
+void printStudInfo(struct student *s, int i);
+void resetGrades();
